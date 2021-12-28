aef2b0f0e5a63990ea5011c4819ad97c8d05d3a0
Dinand van Hartingsveldt <dinand.dotnet@gmail.com>
2021-12-28 15:33:04 +0100

    Add project files.

---
+++ SQLDependencyConsole/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Barmate.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        [Column(TypeName = "decimal(4,2)")]
+        public decimal Price { get; set; }
+        [Column(TypeName = "decimal(4,2)")]
+        public decimal? TakeAwayPrice { get; set; }
+        [Column(TypeName = "decimal(4,2)")]
+        public decimal? DeliveryPrice { get; set; }
+        [Column(TypeName = "decimal(4,2)")]
+        public decimal? VatPercentage { get; set; }
+        public int? Unit { get; set; }
+        public int? CategoryId { get; set; }
+        public DateTime? DateStart { get; set; }
+        public DateTime? DateEnd { get; set; }
+    }
+}
---
+++ SQLDependencyConsole/Program.cs
@@ -0,0 +1,31 @@
+﻿using TableDependency.SqlClient;
+using TableDependency.SqlClient.Base.Enums;
+using Barmate.Models;
+using TableDependency.SqlClient.Base.EventArgs;
+using System.Data.SqlClient;
+
+
+//ref https://www.youtube.com/watch?v=D-ohwsutnPs
+
+string connectionString = "Server=DINAND-NB\\SQLEXPRESS;Database=Barmate;Trusted_Connection=True;MultipleActiveResultSets=true";
+
+using (var tbDependency = new SqlTableDependency<Product>(connectionString))
+{
+    tbDependency.OnChanged += tbDependency_OnChanged;
+    tbDependency.Start();
+    Console.ReadKey();
+
+}
+Console.WriteLine("Hello, World!");
+
+
+static void tbDependency_OnChanged(object sender, TableDependency.SqlClient.Base.EventArgs.RecordChangedEventArgs<Product> e )
+{
+    var entry = e.Entity;
+    Console.WriteLine("Type: " + e.ChangeType);
+    Console.WriteLine("Name"+ entry.Name);
+    Console.WriteLine("Description"+ entry.Description);
+    Console.WriteLine("Price" + entry.Price);
+    Console.WriteLine("TakeawayPrice" + entry.TakeAwayPrice);
+    Console.WriteLine("Active" + entry.VatPercentage);
+}
